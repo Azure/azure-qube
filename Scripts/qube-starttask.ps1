4b708070ffb24c329d48d75fd2cdc54736cc7e8e
Christian Smith <christis@microsoft.com>
2018-10-11 13:05:46 +1100

    Add option to skip install and just set worker group.

--- Scripts/qube-starttask.ps1
+++ Scripts/qube-starttask.ps1
@@ -15,12 +15,8 @@
 #
 Param(
     [string]$qubeSupervisorIp,
-    [string]$domainName,
-    [string]$domainOuPath,
-    [string]$tenantId,
-    [string]$applicationId,
-    [string]$keyVaultCertificateThumbprint,
-    [string]$keyVaultName
+    [string]$workerHostGroups,
+    [switch]$skipInstall
 )
 
 # Set any app licenses system wide.
@@ -34,68 +30,72 @@ if ($env:AZ_BATCH_SOFTWARE_ENTITLEMENT_TOKEN)
 [Environment]::SetEnvironmentVariable("FLEXLM_TIMEOUT", "10000000", "Machine")
 $env:FLEXLM_TIMEOUT = "10000000"
 
-if ('' -eq $domainName)
+if (!$skipInstall)
 {
-    .\join-domain.ps1 -domainName $domainName -domainOuPath $domainOuPath -tenantId $tenantId -applicationId $applicationId -keyVaultCertificateThumbprint $keyVaultCertificateThumbprint -keyVaultName $keyVaultName
+    $python = Get-ChildItem . | where {$_.Name -like "python-2.7.*.amd64.msi"}
+
+    if (!$python)
+    {
+        Write-Host "Could not find a Python 2.7.x installer MSI."
+        exit 1
+    }
+
+    # Install Python
+    Write-Host "Installing " $python.FullName
+    Start-Process msiexec.exe -ArgumentList "/passive /i $($python.FullName)" -Wait
+
+    # Make sure Python is in the path
+    $paths = ${env:PATH}.ToLower().Split(";")
+    if (!$paths.Contains('C:\python27'))
+    {
+        Write-Host "Adding Python to the path."
+        [Environment]::SetEnvironmentVariable("PATH", "C:\Python27;${env:PATH}", "Machine")
+    }
+
+    New-Item -ItemType Directory -Force -Path 'C:\ProgramData\Pfx\Qube' | Out-Null
+
+    # Update the Supervisor IP in qb.conf if specified.
+    if ('' -ne $qubeSupervisorIp)
+    {
+        (Get-Content qb.conf) -replace '^qb_supervisor.*',"qb_supervisor = $qubeSupervisorIp" | Set-Content qb.conf
+    }
+
+    copy qb.conf 'C:\ProgramData\Pfx\Qube'
+
+    # Install Qube Core
+    $qubecore = Get-ChildItem . | where {$_.Name -like "qube-core-*.msi"}
+
+    if (!$qubecore)
+    {
+        Write-Host "Could not find a Qube Core installer MSI."
+        exit 1
+    }
+
+    Write-Host "Installing " $qubecore.FullName
+    Start-Process msiexec.exe -ArgumentList "/passive /i $($qubecore.FullName)" -Wait
+
+    # Install Qube Worker
+    $qubeworker = Get-ChildItem . | where {$_.Name -like "qube-worker-*.msi"}
+
+    if (!$qubeworker)
+    {
+        Write-Host "Could not find a Qube Worker installer MSI."
+        exit 1
+    }
+
+    Write-Host "Installing " $qubeworker.FullName
+    Start-Process msiexec.exe -ArgumentList "/passive /i $($qubeworker.FullName)" -Wait
+
+    # Install Qube Job Type Extras
+    Get-ChildItem . | where { $_.Name -like "qube-*.msi" } | where {$_.Name -notlike "qube-core-*.msi" -and $_.Name -notlike "qube-worker-*.msi"} | % {
+        Write-Host "Installing " $_.FullName
+        Start-Process msiexec.exe -ArgumentList "/passive /i $($_.FullName)" -Wait
+    }
 }
 
-$python = Get-ChildItem . | where {$_.Name -like "python-2.7.*.amd64.msi"}
-
-if (!$python)
+if ('' -ne $workerHostGroups)
 {
-    Write-Host "Could not find a Python 2.7.x installer MSI."
-    exit 1
-}
-
-# Install Python
-Write-Host "Installing " $python.FullName
-Start-Process msiexec.exe -ArgumentList "/passive /i $($python.FullName)" -Wait
-
-# Make sure Python is in the path
-$paths = ${env:PATH}.ToLower().Split(";")
-if (!$paths.Contains('C:\python27'))
-{
-    Write-Host "Adding Python to the path."
-    [Environment]::SetEnvironmentVariable("PATH", "C:\Python27;${env:PATH}", "Machine")
-}
-
-New-Item -ItemType Directory -Force -Path 'C:\ProgramData\Pfx\Qube' | Out-Null
-
-# Update the Supervisor IP in qb.conf if specified.
-if ('' -ne $qubeSupervisorIp)
-{
-    (Get-Content qb.conf) -replace '^qb_supervisor.*',"qb_supervisor = $qubeSupervisorIp" | Set-Content qb.conf
-}
-copy qb.conf 'C:\ProgramData\Pfx\Qube'
-
-# Install Qube Core
-$qubecore = Get-ChildItem . | where {$_.Name -like "qube-core-*.msi"}
-
-if (!$qubecore)
-{
-    Write-Host "Could not find a Qube Core installer MSI."
-    exit 1
-}
-
-Write-Host "Installing " $qubecore.FullName
-Start-Process msiexec.exe -ArgumentList "/passive /i $($qubecore.FullName)" -Wait
-
-# Install Qube Worker
-$qubeworker = Get-ChildItem . | where {$_.Name -like "qube-worker-*.msi"}
-
-if (!$qubeworker)
-{
-    Write-Host "Could not find a Qube Worker installer MSI."
-    exit 1
-}
-
-Write-Host "Installing " $qubeworker.FullName
-Start-Process msiexec.exe -ArgumentList "/passive /i $($qubeworker.FullName)" -Wait
-
-# Install Qube Job Type Extras
-Get-ChildItem . | where { $_.Name -like "qube-*.msi" } | where {$_.Name -notlike "qube-core-*.msi" -and $_.Name -notlike "qube-worker-*.msi"} | % {
-    Write-Host "Installing " $_.FullName
-    Start-Process msiexec.exe -ArgumentList "/passive /i $($_.FullName)" -Wait
+    (Get-Content 'C:\ProgramData\Pfx\Qube\qb.conf') -replace '#worker_groups =.*',"worker_groups = $workerHostGroups" | Set-Content -Force 'C:\ProgramData\Pfx\Qube\qb.conf'
 }
 
 Start-Service -Name "qubeworker"
